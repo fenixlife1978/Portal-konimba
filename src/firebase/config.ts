35e3002e257d71c39411db20771baefe34fc94b3
Edwin Aguiar <fenixlife1978@gmail.com>
2025-12-15 14:28:21 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-6426029066-6f953",
   "appId": "1:370188159320:web:bae5c79c4f238c9911bd2d",
+  "storageBucket": "studio-6426029066-6f953.appspot.com",
   "apiKey": "AIzaSyA_BD97RuTUVwuA5bMKt3eWTXPypoytuPg",
   "authDomain": "studio-6426029066-6f953.firebaseapp.com",
   "measurementId": "",
