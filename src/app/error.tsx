bad7dfcb0df52b4f257051dfd62da6e4ed74a15a
Edwin Aguiar <fenixlife1978@gmail.com>
2025-12-16 02:25:22 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Error({ error }: { error: Error }) {
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <h1 className='text-2xl font-bold'>Something went wrong</h1>
+      <p>{error.message}</p>
+    </div>
+  );
+}
