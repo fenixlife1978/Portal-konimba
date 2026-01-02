b86ef1db1180ba93f33d85646e540d95b2f790b5
Edwin Aguiar <fenixlife1978@gmail.com>
2026-01-02 23:16:57 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/app/admin/(app)/publishers/page.tsx
+++ src/app/admin/(app)/publishers/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import Link from 'next/link';
 import { useCollection } from '@/firebase';
 import { db } from '@/firebase/config';
